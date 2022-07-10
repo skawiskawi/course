25fe20e11f0a60361c35d841e9e2017f5b0bc7d3
skawiskawi <skaaawi@gmail.com>
2022-07-10 21:01:19 +0200

    delete comp, reminder comp

--- angular/src/app/services/task.service.ts
+++ angular/src/app/services/task.service.ts
@@ -5,16 +5,34 @@ import {TASKS} from '../mock-tasks';
 
 import { Observable, of } from 'rxjs';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
   private apiUrl = 'http://localhost:4666/tasks';
+
+
+  
 
   constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task) {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
   }
 }
